d2e6e60f5b02b6b0c81c093fa2ee356bd3d58897
sapere4ude <eileenyoo2@gmail.com>
2021-05-07 22:48:11 +0900

    ch05 다른 뷰 컨트롤러와 데어터 주고받기

---
+++ ch05_SubmitValue/ch05_SubmitValue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ch05_SubmitValue
+//
+//  Created by Kant on 2021/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
