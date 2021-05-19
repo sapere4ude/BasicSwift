69225e81f035e8bd9665338194cd15b5c07be28d
sapere4ude <eileenyoo2@gmail.com>
2021-05-19 17:22:39 +0900

    ch09 Network

--- ch08_tableCellHeight/ch08_tableCellHeight/ListViewController.swift
+++ ch08_tableCellHeight/ch08_tableCellHeight/ListViewController.swift
@@ -9,7 +9,13 @@ import UIKit
 
 class ListViewController: UITableViewController {
     
-    var list = [String]() // 문자열 배열 변수를 담기 위한 객체 (테이블 뷰의 데이터를 저장하기 위함)
+//    var list = [String]() // 문자열 배열 변수를 담기 위한 객체 (테이블 뷰의 데이터를 저장하기 위함)
+    
+    lazy var list: [MovieVO] = {
+        var datalist = [MovieVO]()
+        
+        return datalist
+    }()
     
     @IBAction func add(_ sender: Any) {
         let alert = UIAlertController(title: "목록 입력", message: "추가될 글을 작성해주세요", preferredStyle: .alert)
@@ -64,15 +70,43 @@ class ListViewController: UITableViewController {
         // 호핀 API 호출을 위한 URI 생성
         let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=1&count=10&genreId=&order=releasedateasc"
         
-        let apiURI: URL! = URL(string: url)
+        let apiURI: URL! = URL(string: url)     // URL -> Foundation framework 에 정의된 객체
         
         // REST API 호출
         let apiData = try! Data(contentsOf: apiURI)
         
-        // 데이터 전송 결과를 로그로 출력
-        let log = NSString(data: apiData, encoding: String.Encoding.utf8.rawValue) ?? ""
+        // 데이터 전송 결과를 로그로 출력, NSString(data: <문자열로 변환할 Data 타입 객체>, encoding: <인코딩형식>)
+        let log = NSString(data: apiData, encoding: String.Encoding.utf8.rawValue) ?? ""    // Foundation 에서 지원하는 NSString 타입의 문자열로 반환
         NSLog("API Result = \(log)")
         
+        
+        do {
+            // JSON 객체를 파싱하여 NSDictionary 객체로 받음
+            let apiDictionary = try JSONSerialization.jsonObject(with: apiData, options: []) as! NSDictionary
+            
+            // 데이터 구조에 따라 차례대로 캐스팅하며 읽어온다.
+            let hoppin = apiDictionary["hoppin"] as! NSDictionary   // 키-값으로 구성된 데이터이므로 NSDictionary 타입으로 바꿔준다.
+            let movies = apiDictionary["movies"] as! NSDictionary
+            let movie = apiDictionary["movie"] as! NSArray  // 데이터 형식이 리스트 형태이므로 NSArray 로 타입을 바꿔준다.
+            
+            for row in movie {
+                // 순회 상수를 NSDictionary 타입으로 캐스팅
+                let r = row as! NSDictionary
+                
+                // 테이블 뷰 리스트를 구성할 데이터 형식
+                let mvo = MovieVO()
+                
+                mvo.title = r["title"] as? String
+                mvo.description = r["genreNames"] as? String
+                mvo.thumbnail = r["thumbnailImage"] as? String
+                mvo.detail = r["linkUrl"] as? String
+                mvo.rating = ((r["ratingAverage"] as! NSString).doubleValue)
+                
+                self.list.append(mvo)
+            }
+        } catch {
+            
+        }
     }
 }
 
---
+++ ch09_Network/ch09_Network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ch09_Network
+//
+//  Created by Kant on 2021/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
