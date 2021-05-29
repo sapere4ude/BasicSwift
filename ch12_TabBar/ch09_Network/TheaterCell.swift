c06fcef3b6dec7c58875c7c0a794b60c355d3702
sapere4ude <eileenyoo2@gmail.com>
2021-05-29 13:57:10 +0900

    Tab Bar

---
+++ ch12_TabBar/ch09_Network/TheaterCell.swift
@@ -0,0 +1,14 @@
+//
+//  TheaterCell.swift
+//  ch09_Network
+//
+//  Created by Kant on 2021/05/29.
+//
+
+import UIKit
+class TheaterCell: UITableViewCell {
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var tel: UILabel!
+    @IBOutlet weak var address: UILabel!
+    
+}
